67b27dde76c9db19c56ea638093358fc0e0976ad
Jose-Renato Peña <jose-renato.pena@usach.cl>
2026-06-20 14:49:34 -0400

    registro

--- django/tasks/views.py
+++ django/tasks/views.py
@@ -1,7 +1,12 @@
+#views es la logica, lo que se manda al usuario
+# los emplates son la estructura, rabaja con datos dinamicos
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
 def holamundo(request):
-    return HttpResponse("Hola mundo c:")
+    return render(request,'singup.html', {
+        'form': UserCreationForm
+    })
