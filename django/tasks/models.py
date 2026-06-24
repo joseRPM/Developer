f0378d6b9ff44e72d10c6e24b69b0b57283223e7
Jose-Renato Peña <jose-renato.pena@usach.cl>
2026-06-24 16:23:55 -0400

    tabla base de datos

--- django/tasks/models.py
+++ django/tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User #tabla user
 # Create your models here.
+#tabla sql generada por python
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)      
+    created = models.DateField(auto_now_add=True)
+    datecompleted = models.DateField(null=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
