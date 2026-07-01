7ab2d12e4f428db9a9ee491d63d8df5cc9f3386d
Jose-Renato Peña <jose-renato.pena@usach.cl>
2026-07-01 11:50:35 -0400

    aplicacion estilizada

--- django/tasks/forms.py
+++ django/tasks/forms.py
@@ -1,8 +1,13 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Task
 
-  
-class  taskform(ModelForm):
+#Estilizamos nuestros propios formularios desde aqui
+class  taskform(forms.ModelForm):
     class Meta:
         model = Task
         fields= ['title','description','important']
+        widgets ={
+            'title':forms.TextInput(attrs={'class':'form-control','placeholder':'Escribe el nombre de la tarea'}),
+            'description':forms.Textarea(attrs={'class':'form-control','placeholder':'Escribe la descripcion de la tarea'}),
+            'important':forms.CheckboxInput(attrs={'class': 'form-check-input mx-auto d-block'}),
+        }
